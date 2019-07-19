fe480cec408495cc022a4256a723a17b2a624a99
kanghm0 <hwimin.kang@tu-braunschweig.de>
2019-07-19 19:26:43 +0200

    Koch_A3.1.1_2 Änderung der Klassedefinition
    Klassenattribute und Klassenmethoden wurden geändert.

--- ProjektHHD.cpp
+++ ProjektHHD.cpp
@@ -3,27 +3,15 @@ using namespace std;
 
 class bestellung {                            //Klass Definition
 	private : 
-		int zahl_essen1;                          //Attribute
-		int zahl_essen2;
-		int zahl_essen3;
-		int zahl_essen4;
-		int zahl_essen5;
+		int zahl_essen[5];                          //Attribute
 		int zustand=1;
 	public : 
 		int nr;
 	
 	public :
 		void setzeBestellungsnummer (int ein);
-		void setzeStueckzahl1 (int ein);            //Methode
-		void setzeStueckzahl2 (int ein);
-		void setzeStueckzahl3 (int ein);
-		void setzeStueckzahl4 (int ein);
-		void setzeStueckzahl5 (int ein);
-		int zeigStueckzahl1 (void);
-		int zeigStueckzahl2 (void);
-		int zeigStueckzahl3 (void);
-		int zeigStueckzahl4 (void);
-		int zeigStueckzahl5 (void);
+		void setzeStueckzahl (int index, int ein);            //Methode
+		int zeigStueckzahl (int index);
 		void zustandAendern (int zustand);
 		int zeigZustand (void);
 		void zeigListe(void);
@@ -32,74 +20,36 @@ class bestellung {                            //Klass Definition
 // Methodendefinition
 void bestellung:: setzeBestellungsnummer (int ein) {
 	this->nr = ein; }
-void bestellung:: setzeStueckzahl1 (int ein) {
-	this -> zahl_essen1=ein; }
-void bestellung:: setzeStueckzahl2 (int ein) {
-	this -> zahl_essen2=ein; }
-void bestellung:: setzeStueckzahl3 (int ein) {
-	this -> zahl_essen3=ein; }
-void bestellung:: setzeStueckzahl4 (int ein) {
-	this -> zahl_essen4=ein; }
-void bestellung:: setzeStueckzahl5 (int ein) {
-	this -> zahl_essen5=ein; }
-
-int bestellung:: zeigStueckzahl1 (void) {
-	return zahl_essen1; }
-int bestellung:: zeigStueckzahl2 (void) {
-	return zahl_essen2; }
-int bestellung:: zeigStueckzahl3 (void) {
-	return zahl_essen3; }	
-int bestellung:: zeigStueckzahl4 (void) {
-	return zahl_essen4; }
-int bestellung:: zeigStueckzahl5 (void) {
-	return zahl_essen5; }
+void bestellung:: setzeStueckzahl (int index, int ein) {
+	this -> zahl_essen[index]=ein; }
+int bestellung:: zeigStueckzahl (int index) {
+	return zahl_essen[index]; }
 
 void bestellung:: zustandAendern (int zustand) {       //Statusänderung Methode
 	this-> zustand = zustand; }
 int bestellung::zeigZustand (void) {
 	return zustand;}
 void bestellung:: zeigListe(void) {            //Liste Zeigen Methode
-	cout << nr<<"            "<<zahl_essen1<<"*essen1,  "<< zahl_essen2<<"*essen2,  " <<zahl_essen3<<"*essen3,  "<<zahl_essen4<<"*essen4,  "<<zahl_essen5<<"*essen5"<< "   " << zustand<<endl; }
-		
+	cout << nr<<"            "<<zahl_essen[0]<<"*essen1,  "<< zahl_essen[1]<<"*essen2,  " <<zahl_essen[2]<<"*essen3,  "<<zahl_essen[3]<<"*essen4,  "<<zahl_essen[4]<<"*essen5"<< "   " << zustand<<endl; }
+
+
 int main (void) {
-	int a=3, b=4, c=2, d=1, e=5;	 // Bestellung Stückzahl werden von Kunde-Code bekommen. 
 	int x, y;  //Bei Zustandsänderung
 	int z_aendern;
 	bestellung Bestellung1, Bestellung2, Bestellung3, Bestellung4, Bestellung5;                  // Instanzierung Objekte
-
-	Bestellung1.setzeStueckzahl1(1);                //Für TEST; alle Bestellungsstückzahl 1,2,3,4,5 setzen
-	Bestellung1.setzeStueckzahl2(1);
-	Bestellung1.setzeStueckzahl3(1);
-	Bestellung1.setzeStueckzahl4(1);
-	Bestellung1.setzeStueckzahl5(1);
+	
+	for( int i=0; i<5; i++) {                //Für TEST; alle Bestellungsstückzahl 1,2,3,4,5 setzen
+		Bestellung1.setzeStueckzahl(i,1);
+		Bestellung2.setzeStueckzahl(i,2);
+		Bestellung3.setzeStueckzahl(i,3);
+		Bestellung4.setzeStueckzahl(i,4);
+		Bestellung5.setzeStueckzahl(i,5);
+	}
+	
 	Bestellung1.setzeBestellungsnummer(111);            //Bestellungsnummer wird bei der Bestellung durch Automat aufgeteilt, damit Kunden ihre Bestellungen auf einem Blick erkennen können
-	
-	Bestellung2.setzeStueckzahl1(2);
-	Bestellung2.setzeStueckzahl2(2);
-	Bestellung2.setzeStueckzahl3(2);
-	Bestellung2.setzeStueckzahl4(2);
-	Bestellung2.setzeStueckzahl5(2);
 	Bestellung2.setzeBestellungsnummer(112);
-	
-	Bestellung3.setzeStueckzahl1(3);
-	Bestellung3.setzeStueckzahl2(3);
-	Bestellung3.setzeStueckzahl3(3);
-	Bestellung3.setzeStueckzahl4(3);
-	Bestellung3.setzeStueckzahl5(3);
 	Bestellung3.setzeBestellungsnummer(113);
-	
-	Bestellung4.setzeStueckzahl1(4);
-	Bestellung4.setzeStueckzahl2(4);
-	Bestellung4.setzeStueckzahl3(4);
-	Bestellung4.setzeStueckzahl4(4);
-	Bestellung4.setzeStueckzahl5(4);
 	Bestellung4.setzeBestellungsnummer(114);
-	
-	Bestellung5.setzeStueckzahl1(5);
-	Bestellung5.setzeStueckzahl2(5);
-	Bestellung5.setzeStueckzahl3(5);
-	Bestellung5.setzeStueckzahl4(5);
-	Bestellung5.setzeStueckzahl5(5);
 	Bestellung5.setzeBestellungsnummer(115);
 	
 	cout <<"\nBestellungszustand : " <<endl;            //Zustand
@@ -139,30 +89,30 @@ int main (void) {
 	for(int i=0; i<5; i++) {
 		int j, k;
 		if(Bestellung1.zeigZustand() < Bestellung2.zeigZustand()) {        //Bestellung1<->Bestellung2
-			j=Bestellung1.zeigStueckzahl1();                                            //wenn Bestellungszustand von Bestellung2 höher als 1.
-			k=Bestellung2.zeigStueckzahl1();                                           //Bestellung1 steht immer ganz vorne!!
-			Bestellung1.setzeStueckzahl1(k);
-			Bestellung2.setzeStueckzahl1(j);
+			j=Bestellung1.zeigStueckzahl(0);                                            //wenn Bestellungszustand von Bestellung2 höher als 1.
+			k=Bestellung2.zeigStueckzahl(0);                                           //Bestellung1 steht immer ganz vorne!!
+			Bestellung1.setzeStueckzahl(0,k);
+			Bestellung2.setzeStueckzahl(0,j);
 			
-			j=Bestellung1.zeigStueckzahl2(); 
-			k=Bestellung2.zeigStueckzahl2();
-			Bestellung1.setzeStueckzahl2(k);
-			Bestellung2.setzeStueckzahl2(j);
+			j=Bestellung1.zeigStueckzahl(1); 
+			k=Bestellung2.zeigStueckzahl(1);
+			Bestellung1.setzeStueckzahl(1,k);
+			Bestellung2.setzeStueckzahl(1,j);
 			
-			j=Bestellung1.zeigStueckzahl3(); 
-			k=Bestellung2.zeigStueckzahl3();
-			Bestellung1.setzeStueckzahl3(k);
-			Bestellung2.setzeStueckzahl3(j);
+			j=Bestellung1.zeigStueckzahl(2); 
+			k=Bestellung2.zeigStueckzahl(2);
+			Bestellung1.setzeStueckzahl(2,k);
+			Bestellung2.setzeStueckzahl(2,j);
 			
-			j=Bestellung1.zeigStueckzahl4(); 
-			k=Bestellung2.zeigStueckzahl4();
-			Bestellung1.setzeStueckzahl4(k);
-			Bestellung2.setzeStueckzahl4(j);
+			j=Bestellung1.zeigStueckzahl(3); 
+			k=Bestellung2.zeigStueckzahl(3);
+			Bestellung1.setzeStueckzahl(3,k);
+			Bestellung2.setzeStueckzahl(3,j);
 			
-			j=Bestellung1.zeigStueckzahl5(); 
-			k=Bestellung2.zeigStueckzahl5();
-			Bestellung1.setzeStueckzahl5(k);
-			Bestellung2.setzeStueckzahl5(j);
+			j=Bestellung1.zeigStueckzahl(4); 
+			k=Bestellung2.zeigStueckzahl(4);
+			Bestellung1.setzeStueckzahl(4,k);
+			Bestellung2.setzeStueckzahl(4,j);
 			
 			j=Bestellung1.zeigZustand(); 
 			k=Bestellung2.zeigZustand();
@@ -175,30 +125,30 @@ int main (void) {
 			Bestellung2.nr =j;}
 			
 		if(Bestellung2.zeigZustand() < Bestellung3.zeigZustand()) {        //Bestellung2<->Bestellung3
-			j=Bestellung2.zeigStueckzahl1(); 
-			k=Bestellung3.zeigStueckzahl1();
-			Bestellung2.setzeStueckzahl1(k);
-			Bestellung3.setzeStueckzahl1(j); 
+			j=Bestellung2.zeigStueckzahl(0);
+			k=Bestellung3.zeigStueckzahl(0);
+			Bestellung2.setzeStueckzahl(0,k);
+			Bestellung3.setzeStueckzahl(0,j);
 			
-			j=Bestellung2.zeigStueckzahl2(); 
-			k=Bestellung3.zeigStueckzahl2();
-			Bestellung2.setzeStueckzahl2(k);
-			Bestellung3.setzeStueckzahl2(j); 
+			j=Bestellung2.zeigStueckzahl(1); 
+			k=Bestellung3.zeigStueckzahl(1);
+			Bestellung2.setzeStueckzahl(1,k);
+			Bestellung3.setzeStueckzahl(1,j);
 			
-			j=Bestellung2.zeigStueckzahl3(); 
-			k=Bestellung3.zeigStueckzahl3();
-			Bestellung2.setzeStueckzahl3(k);
-			Bestellung3.setzeStueckzahl3(j); 
+			j=Bestellung2.zeigStueckzahl(2); 
+			k=Bestellung3.zeigStueckzahl(2);
+			Bestellung2.setzeStueckzahl(2,k);
+			Bestellung3.setzeStueckzahl(2,j);
 			
-			j=Bestellung2.zeigStueckzahl4(); 
-			k=Bestellung3.zeigStueckzahl4();
-			Bestellung2.setzeStueckzahl4(k);
-			Bestellung3.setzeStueckzahl4(j); 
+			j=Bestellung2.zeigStueckzahl(3); 
+			k=Bestellung3.zeigStueckzahl(3);
+			Bestellung2.setzeStueckzahl(3,k);
+			Bestellung3.setzeStueckzahl(3,j);
 			
-			j=Bestellung2.zeigStueckzahl5(); 
-			k=Bestellung3.zeigStueckzahl5();
-			Bestellung2.setzeStueckzahl5(k);
-			Bestellung3.setzeStueckzahl5(j); 
+			j=Bestellung2.zeigStueckzahl(4); 
+			k=Bestellung3.zeigStueckzahl(4);
+			Bestellung2.setzeStueckzahl(4,k);
+			Bestellung3.setzeStueckzahl(4,j);
 			
 			j=Bestellung2.zeigZustand(); 
 			k=Bestellung3.zeigZustand();
@@ -211,30 +161,30 @@ int main (void) {
 			Bestellung3.nr =j;}
 			
 		if(Bestellung3.zeigZustand() < Bestellung4.zeigZustand()) {        //Bestellung3<->Bestellung4
-			j=Bestellung3.zeigStueckzahl1(); 
-			k=Bestellung4.zeigStueckzahl1();
-			Bestellung3.setzeStueckzahl1(k);
-			Bestellung4.setzeStueckzahl1(j);
-
-			j=Bestellung3.zeigStueckzahl2(); 
-			k=Bestellung4.zeigStueckzahl2();
-			Bestellung3.setzeStueckzahl2(k);
-			Bestellung4.setzeStueckzahl2(j);
+			j=Bestellung3.zeigStueckzahl(0);
+			k=Bestellung4.zeigStueckzahl(0);
+			Bestellung3.setzeStueckzahl(0,k);
+			Bestellung4.setzeStueckzahl(0,j);
 			
-			j=Bestellung3.zeigStueckzahl3(); 
-			k=Bestellung4.zeigStueckzahl3();
-			Bestellung3.setzeStueckzahl3(k);
-			Bestellung4.setzeStueckzahl3(j);
+			j=Bestellung3.zeigStueckzahl(1); 
+			k=Bestellung4.zeigStueckzahl(1);
+			Bestellung3.setzeStueckzahl(1,k);
+			Bestellung4.setzeStueckzahl(1,j);
 			
-			j=Bestellung3.zeigStueckzahl4(); 
-			k=Bestellung4.zeigStueckzahl4();
-			Bestellung3.setzeStueckzahl4(k);
-			Bestellung4.setzeStueckzahl4(j);
+			j=Bestellung3.zeigStueckzahl(2); 
+			k=Bestellung4.zeigStueckzahl(2);
+			Bestellung3.setzeStueckzahl(2,k);
+			Bestellung4.setzeStueckzahl(2,j);
 			
-			j=Bestellung3.zeigStueckzahl5(); 
-			k=Bestellung4.zeigStueckzahl5();
-			Bestellung3.setzeStueckzahl5(k);
-			Bestellung4.setzeStueckzahl5(j);
+			j=Bestellung3.zeigStueckzahl(3); 
+			k=Bestellung4.zeigStueckzahl(3);
+			Bestellung3.setzeStueckzahl(3,k);
+			Bestellung4.setzeStueckzahl(3,j);
+			
+			j=Bestellung3.zeigStueckzahl(4); 
+			k=Bestellung4.zeigStueckzahl(4);
+			Bestellung3.setzeStueckzahl(4,k);
+			Bestellung4.setzeStueckzahl(4,j);
 			
 			j=Bestellung3.zeigZustand(); 
 			k=Bestellung4.zeigZustand();
@@ -247,30 +197,30 @@ int main (void) {
 			Bestellung4.nr =j;}
 			
 		if(Bestellung4.zeigZustand() < Bestellung5.zeigZustand()) {        //Bestellung4<->Bestellung5
-			j=Bestellung4.zeigStueckzahl1(); 
-			k=Bestellung5.zeigStueckzahl1();
-			Bestellung4.setzeStueckzahl1(k);
-			Bestellung5.setzeStueckzahl1(j); 
+			j=Bestellung4.zeigStueckzahl(0);
+			k=Bestellung5.zeigStueckzahl(0);
+			Bestellung4.setzeStueckzahl(0,k);
+			Bestellung5.setzeStueckzahl(0,j);
 			
-			j=Bestellung4.zeigStueckzahl2(); 
-			k=Bestellung5.zeigStueckzahl2();
-			Bestellung4.setzeStueckzahl2(k);
-			Bestellung5.setzeStueckzahl2(j); 
+			j=Bestellung4.zeigStueckzahl(1); 
+			k=Bestellung5.zeigStueckzahl(1);
+			Bestellung4.setzeStueckzahl(1,k);
+			Bestellung5.setzeStueckzahl(1,j);
 			
-			j=Bestellung4.zeigStueckzahl3(); 
-			k=Bestellung5.zeigStueckzahl3();
-			Bestellung4.setzeStueckzahl3(k);
-			Bestellung5.setzeStueckzahl3(j); 
+			j=Bestellung4.zeigStueckzahl(2); 
+			k=Bestellung5.zeigStueckzahl(2);
+			Bestellung4.setzeStueckzahl(2,k);
+			Bestellung5.setzeStueckzahl(2,j);
 			
-			j=Bestellung4.zeigStueckzahl4(); 
-			k=Bestellung5.zeigStueckzahl4();
-			Bestellung4.setzeStueckzahl4(k);
-			Bestellung5.setzeStueckzahl4(j); 
+			j=Bestellung4.zeigStueckzahl(3); 
+			k=Bestellung5.zeigStueckzahl(3);
+			Bestellung4.setzeStueckzahl(3,k);
+			Bestellung5.setzeStueckzahl(3,j);
 			
-			j=Bestellung4.zeigStueckzahl5(); 
-			k=Bestellung5.zeigStueckzahl5();
-			Bestellung4.setzeStueckzahl5(k);
-			Bestellung5.setzeStueckzahl5(j); 
+			j=Bestellung4.zeigStueckzahl(4); 
+			k=Bestellung5.zeigStueckzahl(4);
+			Bestellung4.setzeStueckzahl(4,k);
+			Bestellung5.setzeStueckzahl(4,j);
 			
 			j=Bestellung4.zeigZustand(); 
 			k=Bestellung5.zeigZustand();
@@ -283,6 +233,7 @@ int main (void) {
 			Bestellung5.nr =j;}
 		}
 	
+	
 	cout << "\n<Bestellungsliste>\n";
 	cout << "Bestellung     Stueckzahl                                             Zustand\n";
 	Bestellung1.zeigListe();
@@ -290,8 +241,8 @@ int main (void) {
 	Bestellung3.zeigListe();
 	Bestellung4.zeigListe();
 	Bestellung5.zeigListe();
-	
-	if(Bestellung5.zeigZustand() == 0) {                       //bool funktion
+		
+	if(Bestellung5.zeigZustand() == 0) {
 		if(Bestellung4.zeigZustand() == 0) {
 			if(Bestellung3.zeigZustand() == 0) {
 				if(Bestellung2.zeigZustand() == 0) {
@@ -339,6 +290,5 @@ int main (void) {
 		Bestellung4.zeigListe();
 		Bestellung5.zeigListe();	
 	}
-	
 	return 0;
 }
